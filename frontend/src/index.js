6a01594a0f28e30c8dbd01c39151512f850fa938
Marianna Patrusova <sumeragi.tanako@gmail.com>
2020-08-21 14:35:38 +0300

    yup and formic validation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
+        <MuiThemeProvider>
         <App/>
+        </MuiThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
