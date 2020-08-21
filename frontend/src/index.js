be7d27abc86b2c98c2b802eee0996b8904b1132e
Marianna Patrusova <sumeragi.tanako@gmail.com>
2020-08-21 19:59:50 +0300

    little non-functional refactoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <React.StrictMode>
         <MuiThemeProvider>
-        <App/>
+            <App/>
         </MuiThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
