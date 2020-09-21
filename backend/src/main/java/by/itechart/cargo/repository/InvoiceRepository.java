ca07645b1b02aa08739fdd412030df40f3017def
Revollutiion <vladmironuk@gmail.com>
2020-09-22 01:01:51 +0300

    add "edit rejected invoice" feature to backend

--- backend/src/main/java/by/itechart/cargo/repository/InvoiceRepository.java
+++ backend/src/main/java/by/itechart/cargo/repository/InvoiceRepository.java
@@ -5,8 +5,11 @@ import by.itechart.cargo.model.freight.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findByClientCompany(ClientCompany clientCompany);
+
+    Optional<Invoice> findByNumber(String number);
 }
