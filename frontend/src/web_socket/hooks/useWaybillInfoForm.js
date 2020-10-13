4691c6d59f10cbe7ec208c26992c10cf452d5dfd
Revollutiion <vladmironuk@gmail.com>
2020-10-13 13:46:32 +0300

    add waybill info hook

---
+++ frontend/src/web_socket/hooks/useWaybillInfoForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import {DialogWindow} from "../../components/parts/dialogs/dialog";
+import {InvoiceInfo} from "../../components/roles/manager/invoice-info";
+import {WaybillInfo} from "../../components/roles/driver/waybill-info";
+
+export default () => {
+    const [open, setOpen] = useState(false);
+    const [waybillId, setWaybillId] = useState(-1)
+
+    const handleDialogClose = () => {
+        setOpen(false);
+    }
+
+    const openWaybillInfoDialog = (waybillId) => {
+        setWaybillId(waybillId);
+        setOpen(true)
+    }
+
+    const WaybillInfoDialogWithForm = (
+        <DialogWindow
+            dialogTitle={"Waybill"}
+            fullWidth={true}
+            maxWidth="xl"
+            handleClose={handleDialogClose}
+            openDialog={open}
+            form={<WaybillInfo waybillId={waybillId} onClose={handleDialogClose}/>}
+        />
+    )
+
+    return [WaybillInfoDialogWithForm, openWaybillInfoDialog];
+}
