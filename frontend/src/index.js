137f4b3e898f9362d010164693e8a2b0ca1896c5
Revollutiion <vladmironuk@gmail.com>
2020-11-23 17:59:51 +0300

    deploy test component on android

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {createStore} from "redux";
-import {Provider} from "react-redux"
+// import './index.css';
+// import App from './components/App';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import {createStore} from "redux";
+// import {Provider} from "react-redux"
 import {rootReducer} from "./components/store/reducers";
-import {BrowserRouter} from "react-router-dom";
-import {SnackbarProvider} from "notistack";
+// import {BrowserRouter} from "react-router-dom";
+// import {SnackbarProvider} from "notistack";
+import {Text} from "react-native"
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
-ReactDOM.render(
-    <MuiThemeProvider>
-        <SnackbarProvider
-            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-            maxSnack={5}
-        >
-            <Provider store={store}>
-                <BrowserRouter>
-                    <App/>
-                </BrowserRouter>
-            </Provider>
-        </SnackbarProvider>
-    </MuiThemeProvider>,
+ReactDOM.render(<Text>Some text</Text>,
+    // <MuiThemeProvider>
+    //     <SnackbarProvider
+    //         anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+    //         maxSnack={5}
+    //     >
+    //         <Provider store={store}>
+    //             <BrowserRouter>
+    //                 <App/>
+    //             </BrowserRouter>
+    //         </Provider>
+    //     </SnackbarProvider>
+    // </MuiThemeProvider>,
     document.getElementById('root')
 );
 
